ca355d2f1ddb974345ee645337074190bee859b6
Emil Örn <emmikall95@gmail.com>
2021-02-08 22:26:50 +0000

    The latest stuff

---
+++ AGDDPlatformer-master/Assets/Scripts/Gate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.IgnoreLayerCollision();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}

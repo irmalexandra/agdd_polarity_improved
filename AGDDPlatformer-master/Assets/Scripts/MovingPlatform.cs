7f49f6e9922e1f546bdd26a60df4d6c3167e96e4
Emil Örn <emmikall95@gmail.com>
2021-02-22 21:55:42 +0000

    broken moving platforms

--- AGDDPlatformer-master/Assets/Scripts/MovingPlatform.cs
+++ AGDDPlatformer-master/Assets/Scripts/MovingPlatform.cs
@@ -9,29 +9,61 @@ public class MovingPlatform : MonoBehaviour
     public Transform pos1;
     public Transform pos2;
     public Transform startPos;
-    
+    public float stop_timer;
     public float speed;
 
+    private float _timer;
     public Vector3 nextPos;
+    private bool stop;
 
     // Start is called before the first frame update
     void Start()
     {
         nextPos = startPos.position;
+        stop = false;
+        _timer = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position == pos1.position)
+        if (!stop)
         {
+            transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+            if(_timer > )
+        }
+        else if(stop)
+        {
+            Debug.Log(_timer);
+            _timer += Time.deltaTime;
+            if (_timer >= stop_timer)
+            {
+                stop = false;
+            }
+        }
+        else
+        {
+            stop = false;
+        }
+        
+        if (transform.position == pos1.position && _timer <= 0)
+        {
+            _timer = stop_timer;
+            stop = true;
             nextPos = pos2.position;
         }    
-        if (transform.position == pos2.position)
+        if (transform.position == pos2.position && _timer <= 0)
         {
+            stop = true;
+            _timer = stop_timer;
             nextPos = pos1.position;
         }
-        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+        
+        
+
+
+
     }
 
     private void OnDrawGizmos()
